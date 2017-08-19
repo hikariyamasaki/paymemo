e783526d6fe16d4cf147ca33eb3b461749e7bba8
Yu Yamashiro <yuuyamashiro@gmail.com>
2017-08-19 15:07:32 +0900

    make controller.js. change div -> form

---
+++ controller.js
@@ -0,0 +1,14 @@
+
+
+function okPushed() {
+
+  var y = Number(document.input_area.daysY.value)
+  var m = Number(document.input_area.daysM.value)
+  var d = Number(document.input_area.daysD.value)
+  var staff = Number(document.input_area.staff.value)
+  var yu = Number(document.input_area.yu.value)
+  var hika = Number(document.input_area.hikari.value)
+
+  
+
+}
